d1e7461930f1d712f07454dfe6135c623fa5cc14
Matthew Farejowicz <mfarejow@mit.edu>
2020-01-13 14:02:25 -0500

    add 404 styles

--- components/NotFound/NotFound.jsx
+++ components/NotFound/NotFound.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 
+import styles from "./NotFound.css";
+
 class NotFound extends React.Component {
   render() {
     return (
-      <div>
+      <div style={styles.section}>
         <h1>Page not found!</h1>
         <div>
           Click <a href="/">here</a> to head back home
